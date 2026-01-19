269dd5b71019a2e3e6929828faa163fa6a861104
kalimuthu4978 <kalimuthu4978@gmail.com>
2026-01-19 18:58:29 +0530

    Mid class udpate

--- class.js
+++ class.js
@@ -1,2 +1,28 @@
-let a  =25;
-console.log(a); 
+/*let a  =25;
+console.log(a); */
+
+
+/*let number = [3,5,1,4,2];
+
+function compareFn(a,b){
+    console.log(`a: ${a}, b: ${b}`);
+    if(a<b){
+        return -1;
+    }else if(a>b){
+        return 1;
+    }else if(a==b){
+        return 0;
+    }
+}
+
+number.sort(compareFn);
+
+console.log(number);*/
+
+let words = ['banana', 'apple', 'orange', 'kiwi', 'grape', 'mango', 'strawberry', 'blueberry'];
+
+let index = 0;
+while(index < words.length){
+    console.log(words[index]);
+    index++;
+}
