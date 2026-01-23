3abbd2c6f2fcdfa94919b47b4cd0f01172bcaf38
kalimuthu4978 <kalimuthu4978@gmail.com>
2026-01-23 20:42:31 +0530

    External Java script added

---
+++ Tailwind CSS/JSSCRITP.js
@@ -0,0 +1,11 @@
+let fruits = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry', 'Fig', 'Grape', 'Honeydew', 'Indian Fig', 'Jackfruit'];
+const h1 = document.createElement('h1');
+h1.textContent = "List of Fruits";
+
+const ul = document.createElement('ul');
+fruits.forEach(fruit =>{
+    const li = document.createElement("li");
+    li.textContent = fruit;
+    ul.appendChild(li);
+            })
+document.body.append(h1, ul);
