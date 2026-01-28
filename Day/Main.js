fb66b7659814c6058f251afbbfc83ff54683b66d
kalimuthu4978 <kalimuthu4978@gmail.com>
2026-01-28 20:42:39 +0530

    Prmose then and catch

--- Day/Main.js
+++ Day/Main.js
@@ -1,14 +1,21 @@
 const promise = new Promise((resolve, reject) => { 
-    reject()
+    resolve() // will call the fuction from below .then 
 } );  // it needs an executor and rejector /*
 
 promise
     .then (
-        // success call back funciton
+        // success call back funciton assigned to resolve 
         () => {
             console.log("operation success")
         },
+        // // failure call back funciton assigned to reject
+        // () => {
+        //     console.log("Operation failed")
+        // }
+    )
+    // we cna use the cathc to get the reject statement 
+    .catch(
         () => {
-            console.log("Operation failed")
+            console.log("operation failed")
         }
     )
