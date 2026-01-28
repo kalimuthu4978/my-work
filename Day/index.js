93b3ccb37509626b3e4d79f871a6cb7c2032c2ca
kalimuthu4978 <kalimuthu4978@gmail.com>
2026-01-28 20:49:09 +0530

    java scritpt promises

---
+++ Day/index.js
@@ -0,0 +1,26 @@
+const promise = new Promise((resolve, reject) => { 
+    setTimeout(() => {
+        resolve();
+    }, 3000)
+
+} );  // it needs an executor and rejector /*
+
+promise
+    .then (
+        // success call back funciton assigned to resolve 
+        () => {
+            console.log("operation success")
+        },
+        // // failure call back funciton assigned to reject
+        // () => {
+        //     console.log("Operation failed")
+        // }
+    )
+    // we cna use the cathc to get the reject statement 
+    .catch(
+        () => {
+            console.log("operation failed")
+        }
+    )
+
+console.log(promise)
